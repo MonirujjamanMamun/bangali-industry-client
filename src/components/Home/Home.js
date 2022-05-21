ee468468cb996569f5a99a3b8423f0b3c265e69b
Monirujjaman Mamun <96871498+MonirujjamanMamun@users.noreply.github.com>
2022-05-21 23:39:23 +0600

    all file created

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navebar from '../Shared/Navebar';
+
+const Home = () => {
+    return (
+        <div>
+            <Navebar></Navebar>
+            <h1>This is home</h1>
+        </div>
+    );
+};
+
+export default Home;
