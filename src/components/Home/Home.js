c050b6a6773144df987407912d3cb60f693fccdd
Monirujjaman Mamun <96871498+MonirujjamanMamun@users.noreply.github.com>
2022-05-22 02:13:02 +0600

    footer added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Footer from '../Shared/Footer';
 import Banner from './Banner';
 
 
@@ -7,6 +8,7 @@ const Home = () => {
         <div>
 
             <Banner></Banner>
+            <Footer></Footer>
         </div>
     );
 };
