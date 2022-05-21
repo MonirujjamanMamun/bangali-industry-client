81de95fe2548d5da45d892b7d495ab6b939c89da
Monirujjaman Mamun <96871498+MonirujjamanMamun@users.noreply.github.com>
2022-05-22 00:58:21 +0600

    banner added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Banner from './Banner';
 
 
 const Home = () => {
     return (
         <div>
-            <h1>This is home</h1>
+
+            <Banner></Banner>
         </div>
     );
 };
